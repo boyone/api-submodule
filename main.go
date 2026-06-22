14beae8e03fbf8d098d3fb8963af2ac40262b616
boyone <boyone.g@gmail.com>
2026-06-22 17:22:20 +0700

    update to v.4

--- main.go
+++ main.go
@@ -27,7 +27,7 @@ func main() {
 
 func helloHandler(w http.ResponseWriter, r *http.Request) {
 	writeJSON(w, http.StatusOK, map[string]string{
-		"message": "Hello, World API3!",
+		"message": "Hello, World API4!",
 	})
 }
 
