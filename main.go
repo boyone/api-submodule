091d8c69deebf070275b46c899d8cc332aa4fd9f
boyone <boyone.g@gmail.com>
2026-06-22 16:21:55 +0700

    update message

--- main.go
+++ main.go
@@ -27,7 +27,7 @@ func main() {
 
 func helloHandler(w http.ResponseWriter, r *http.Request) {
 	writeJSON(w, http.StatusOK, map[string]string{
-		"message": "Hello, World API!",
+		"message": "Hello, World API2!",
 	})
 }
 
