8de58376981797b6b7c8f874acb3bca1588da6bc
boyone <boyone.g@gmail.com>
2026-06-24 14:19:27 +0700

    update to v11

--- main.go
+++ main.go
@@ -27,7 +27,7 @@ func main() {
 
 func helloHandler(w http.ResponseWriter, r *http.Request) {
 	writeJSON(w, http.StatusOK, map[string]string{
-		"message": "Hello, World API10!",
+		"message": "Hello, World API11!",
 	})
 }
 
